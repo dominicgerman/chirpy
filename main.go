248597a927ad2bcb093f71c1172e580c4911a169
Dominic German <dominicgerman@gmail.com>
2024-06-09 20:19:40 -0500

    routing, json

--- main.go
+++ main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
-) 
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const port = "8080"
+	const filePathRoot = "."
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
 
 	mux := http.NewServeMux()
 
+	// Wrap the file server with the middleware
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	// Create and configure the server
 	server := &http.Server{
-		Addr: "127.0.0.1:" + port,
+		Addr:    "127.0.0.1:" + port,
 		Handler: mux,
 	}
 
---
+++ readiness.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=UTF-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
